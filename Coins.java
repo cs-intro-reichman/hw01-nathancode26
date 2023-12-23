b0504e86c10ee01550ddb99bf21868c90ff95710
albon <nathan.albo1@mail.huji.ac.il>
2023-12-23 19:14:42 +0200

    commit 233

--- Coins.java
+++ Coins.java
@@ -21,5 +21,6 @@ public class Coins {
             number_cent = number;
         }
         System.out.println("Use " + number_quarter + " quarters and " +  number_cent + " cents");
+        
 	}
 }
